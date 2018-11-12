b6437e4b0649186e442677f51c781d6553921f55
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-12 02:42:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ function logDriverNames(driver){
 }
 
 function logDriversByHometown(driver,homeTown){
- let htArray= driver.filter(d){
+ let htArray= driver.filter(function (d){
    return d.hometown === homeTown
- }
+ })
  htArray.forEach(function(d){
    return console.log(d.name)
  })
 }
+
